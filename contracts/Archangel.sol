d999807e16a14a839ea8489eb6c16f1303319b0a
Jez Higgins <jez@jezuk.co.uk>
2017-10-10 14:51:20 +0100

    Write and read tests

--- contracts/Archangel.sol
+++ contracts/Archangel.sol
@@ -5,12 +5,32 @@ contract Archangel {
   mapping (address => bool) public permissions;
 
   event Registration(string _key, string _payload);
+  event NoWritePermission(address _addr);
+  event DuplicateKey(string _key);
 
   function Archangel() {
     permissions[tx.origin] = true;
-  }
+  } // Archangel
 
   function hasPermission(address addr) constant returns(bool) {
     return permissions[addr];
-  }
-}
+  } // hasPermissions
+
+  function store(string key, string payload) {
+    if (!permissions[tx.origin]) {
+      NoWritePermission(tx.origin);
+      return;
+    }
+    if (bytes(registry[key]).length != 0) {
+      DuplicateKey(key);
+      return;
+    }
+
+    registry[key] = payload;
+    Registration(key, payload);
+  } // store
+
+  function fetch(string key) constant returns(string) {
+    return registry[key];
+  } // fetch
+} // contract Archangel
--- test/archangel.js
+++ test/archangel.js
@@ -3,7 +3,7 @@ const Archangel = artifacts.require('./Archangel.sol');
 contract('Archangel', (accounts) => {
   it('contract owner has write permission', () => {
     return Archangel.deployed().then(instance => {
-      console.log(instance.abi)
+      //console.log(instance.abi)
       return instance.hasPermission(accounts[0])
     }).then(permission => {
       assert(typeof permission === 'boolean')
@@ -18,5 +18,35 @@ contract('Archangel', (accounts) => {
       assert(typeof permission === 'boolean')
       assert(!permission)
     });
+  });
+
+  it('owner stores a payload and then retrieves it', async () => {
+    const instance = await Archangel.deployed();
+
+    const tx = await instance.store('fruit', 'melon');
+
+    const payload = await instance.fetch('fruit');
+
+    assert.equal(payload, 'melon')
+  });
+
+  it('non-owner cannot write', async() => {
+    const instance = await Archangel.deployed();
+
+    const tx = await instance.store('pet', 'lovely black dog', { from: accounts[2] });
+
+    const payload = await instance.fetch('pet', { from: accounts[2] });
+
+    assert.equal(payload, '')
+  });
+
+  it('non-owner can read', async() => {
+    const instance = await Archangel.deployed();
+
+    const tx = await instance.store('insect', 'housefly');
+
+    const payload = await instance.fetch('insect', { from: accounts[2] });
+
+    assert.equal(payload, 'housefly')
   });
 });
