2f21592159ed2cc4c2d6940691bc4d4b766441da
Jez Higgins <jez@jezuk.co.uk>
2018-06-08 13:35:00 +0100

    reorganised tests a little

--- test/archangel.js
+++ test/archangel.js
@@ -8,95 +8,99 @@ contract('Archangel', (accounts) => {
   const reader = accounts[1];
   const other_actor = accounts[2];
 
-  it('contract owner has write permission', async () => {
-    const instance = await Archangel.deployed();
+  describe('permissions check', () => {
+    it('contract owner has write permission', async () => {
+      const instance = await Archangel.deployed();
 
-    const permission = await instance.hasPermission(owner)
+      const permission = await instance.hasPermission(owner)
 
-    assert(typeof permission === 'boolean')
-    assert(permission)
-  });
+      assert(typeof permission === 'boolean')
+      assert(permission)
+    })
 
-  it('non-contract owner doesn\'t have write permission', async () => {
-    const instance = await Archangel.deployed();
+    it('non-contract owner doesn\'t have write permission', async () => {
+      const instance = await Archangel.deployed();
 
-    const permission = await instance.hasPermission(reader)
+      const permission = await instance.hasPermission(reader)
 
-    assert(typeof permission === 'boolean')
-    assert(!permission)
-  });
+      assert(typeof permission === 'boolean')
+      assert(!permission)
+    })
+  })
 
-  it('owner stores a payload and then retrieves it', async () => {
-    const instance = await Archangel.deployed();
+  describe('writing a payload', () => {
+    it('owner stores a payload and then retrieves it', async () => {
+      const instance = await Archangel.deployed();
 
-    await instance.store('fruit', 'melon');
+      await instance.store('fruit', 'melon');
 
-    const payload = await instance.fetch('fruit');
+      const payload = await instance.fetch('fruit');
 
-    assert.equal(payload[0], 'melon');
-  });
+      assert.equal(payload[0], 'melon');
+    })
 
-  it('non-owner cannot write', async() => {
-    const instance = await Archangel.deployed();
+    it('non-owner cannot write', async() => {
+      const instance = await Archangel.deployed();
 
-    await instance.store('pet', 'lovely black dog', { from: other_actor });
+      await instance.store('pet', 'lovely black dog', { from: other_actor });
 
-    const payload = await instance.fetch('pet', { from: other_actor });
+      const payload = await instance.fetch('pet', { from: other_actor });
 
-    assert.equal(payload[0], '')
-  });
+      assert.equal(payload[0], '')
+    })
 
-  it('non-owner has permission once granted', async() => {
-    const instance = await Archangel.deployed();
+    it('accounts without write permission can read', async() => {
+      const instance = await Archangel.deployed();
 
-    const initial_permission = await instance.hasPermission(other_actor);
-    assert(!initial_permission);
+      const payload = await instance.fetch('fruit', { from: reader });
 
-    await instance.grantPermission(other_actor);
-    const write_permission_granted = await instance.hasPermission(other_actor);
-    assert(write_permission_granted);
+      assert.equal(payload[0], 'melon')
+    })
+  })
 
-    await instance.removePermission(other_actor);
-    const write_permission_revoked = await instance.hasPermission(other_actor);
-    assert(!write_permission_revoked);
-});
+  describe('can write once permission granted', () => {
+    it('non-owner has permission once granted', async() => {
+      const instance = await Archangel.deployed();
 
-  it('non-owner can write when given permission', async() => {
-    const instance = await Archangel.deployed();
+      const initial_permission = await instance.hasPermission(other_actor);
+      assert(!initial_permission);
 
-    await instance.grantPermission(other_actor);
+      await instance.grantPermission(other_actor);
+      const write_permission_granted = await instance.hasPermission(other_actor);
+      assert(write_permission_granted);
 
-    await instance.store('grain', 'barley', { from: other_actor });
+      await instance.removePermission(other_actor);
+      const write_permission_revoked = await instance.hasPermission(other_actor);
+      assert(!write_permission_revoked);
+    })
 
-    const payload = await instance.fetch('grain', { from: other_actor });
+    it('non-owner can write when given permission', async() => {
+      const instance = await Archangel.deployed();
 
-    assert.equal(payload[0], 'barley')
-  });
+      await instance.grantPermission(other_actor);
 
-  it('non-owner can not write when permission revoked', async() => {
-    const instance = await Archangel.deployed();
+      await instance.store('grain', 'barley', { from: other_actor });
 
-    await instance.grantPermission(other_actor);
-    await instance.store('tea', 'assam', { from: other_actor });
-    const tea = await instance.fetch('tea', { from: other_actor });
-    assert.equal(tea[0], 'assam')
+      const payload = await instance.fetch('grain', { from: other_actor });
 
-    await instance.removePermission(other_actor);
-    await instance.store('coffee', 'takengon', { from: other_actor });
-    const coffee = await instance.fetch('coffee', { from: other_actor });
-    assert.equal(coffee[0], '')
-  });
+      assert.equal(payload[0], 'barley')
+    })
 
-  it('non-owner can read', async() => {
-    const instance = await Archangel.deployed();
+    it('non-owner can not write when permission revoked', async() => {
+      const instance = await Archangel.deployed();
 
-    await instance.store('insect', 'housefly');
+      await instance.grantPermission(other_actor);
+      await instance.store('tea', 'assam', { from: other_actor });
+      const tea = await instance.fetch('tea', { from: other_actor });
+      assert.equal(tea[0], 'assam')
 
-    const payload = await instance.fetch('insect', { from: reader });
-
-    assert.equal(payload[0], 'housefly')
-  });
-});
+      await instance.removePermission(other_actor);
+      await instance.store('coffee', 'takengon', { from: other_actor });
+      const coffee = await instance.fetch('coffee', { from: other_actor });
+      assert.equal(coffee[0], '')
+    })
+  })
+})
 
 
 contract('can store multiple identical payloads per key', () => {
