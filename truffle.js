cab07df89b59e959ec5a09d45f76ccd00cc03e9c
Jez Higgins <jez@jezuk.co.uk>
2018-06-25 11:11:27 +0100

    Added archangel network details

--- truffle.js
+++ truffle.js
@@ -3,7 +3,13 @@ module.exports = {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: "3" // Match any network id
+    },
+    archangel: {
+      host: "localhost",
+      port: 8545,
+      network_id: 3151,
+      from: "0x9071fe4e16752193d03e9d0b2a5c0f4db5a3c70f"
     }
   }
-};
+}
